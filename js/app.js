c184c5be827d6b2f809804f353d551ac5742e4ac
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-06 15:31:31 -0800

    JS updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 'use strict';
 
-var kids = prompt('how may kids does brian have?');
-console.log('number of kids:', kids);
+var user = prompt('Hello! What is your name?');
+console.log('name of user:', user);
 
-var guitars = prompt('how many guitars does brian have?');
-console.log('num guitars:', guitars);
+var school = prompt('Did you attend University?');
+console.log('education:', school);
+
+var job = prompt('Have you had a career before?');
+console.log('work history:', job);
+
+var travel = prompt('Have you traveled a lot?');
+console.log('travel:', travel);
+
+var goal = prompt('Do you plan on going to University?');
+console.log('school goals:', goal);
+
+var tech = prompt('Do you want to work in tech?');
+console.log('tech job:', tech);
 
 if (kids === '3' || kids === '4') {
   console.log('kids eval:', 'great');

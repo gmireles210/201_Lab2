c289fec3d46c9b23593722f4587dd8c2e74e866a
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-06 19:56:13 -0800

    update

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ if (travel.toLowerCase() === 'y' || travel.toLowerCase() === 'yes') {
 
 var snow = prompt('How many countries have I snowboarded in?');
 //console.log('snow goals:', goal);
-if (snow === '7' || snow.toUpperCase() === 'seven') {
+if (snow === '7') {
   alert('Yes, I have been snowboarding in 7 different countries');
 } else {
   alert('Wrong, I have been snowboarding in 7 different countries');

11331ec8f91674ff795699baf4be28fc37cfd563
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-07 14:35:12 -0800

    adjusted counter

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ while(number !== 13) {
   } else if (number === number[13]) {
     alert('nice job, my fav number is ' + '' + number[13]);
   } else if (counter > 4) {
-    alert('Too many attempts, my fav number is ' + '' + number[13]);
+    alert('Too many attempts, my fav number is 13');
   }
 }
 alert('current count:', counter);

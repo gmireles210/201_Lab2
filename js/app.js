6c42e006d80d0234386bcc50e052d0ce286f8ccd
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-06 16:09:20 -0800

    Revised JS

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@
 var user = prompt('Hello! What is your name?');
 console.log('name of user:', user);
 
-var school = prompt('Did you attend University?');
+var school = prompt('Where did I go to school?');
 console.log('education:', school);
 
-var job = prompt('Have you had a career before?');
+var job = prompt('Was I in the Army?');
 console.log('work history:', job);
 
-var travel = prompt('Have you traveled a lot?');
+var travel = prompt('Have I traveled a lot?');
 console.log('travel:', travel);
 
-var goal = prompt('Do you plan on going to University?');
+var goal = prompt('Did I go to graduate school?');
 console.log('school goals:', goal);
 
-var tech = prompt('Do you want to work in tech?');
+var tech = prompt('Do I work in the tech industry?');
 console.log('tech job:', tech);
 
 if (school === 'y' || school === 'Y') {

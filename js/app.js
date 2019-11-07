4b679886a2750812a901193862b642886113f42b
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-06 19:39:39 -0800

    adjustments

--- js/app.js
+++ js/app.js
@@ -1,58 +1,47 @@
-<<<<<<< HEAD
 'use strict';
 
 var user = prompt('Hello! What is your name?');
-console.log('name of user:', user);
 
-var school = prompt('Did I go to school at UW?');
-console.log('education:', school);
+var school = prompt('Did I go to school at UW? Please answer with a yes or no');
+// console.log('education:', school);
+if (school.toLowerCase() === 'y' || school.toLowerCase() === 'yes') {
+  alert('Wrong answer to this question');
+} else {
+  alert('Correct! I went to the University of Washington');
+}
 
-var job = prompt('Was I in the Army?');
-console.log('work history:', job);
+var job = prompt('How many years was I in the Army?');
+//console.log('work history:', job);
+if (job === '10' || job === 'ten') {
+  alert('Correct!, I was in the Army);
+} else {
+  alert('Wrong answer to this question');
+}
 
 var travel = prompt('Have I traveled a lot?');
-console.log('travel:', travel);
-
-var goal = prompt('Did I go to graduate school?');
-console.log('school goals:', goal);
-
-var tech = prompt('Do I work in the tech industry?');
-console.log('tech job:', tech);
-
-if (school.toUpperCase() === 'yes' || school === 'YES') {
-  console.log('Went to school:', 'great');
-  // do stuff here
+//console.log('travel:', travel);
+if (travel.toLowerCase() === 'y' || travel.toLowerCase() === 'yes') {
+  alert('Yes, I have traveled a lot!');
+} else {
+  alert('Wrong, I have traveled a lot.);
 }
 
-if (school === 'n' && goal === 'y') {
-  console.log('Higher Education:', 'False')
-=======
-'use strict';
-
-var user = prompt('Hello! What is your name?');
-console.log('name of user:', user);
-
-var school = prompt('Did I go to school at UW?');
-console.log('education:', school);
-
-var job = prompt('Was I in the Army?');
-console.log('work history:', job);
-
-var travel = prompt('Have I traveled a lot?');
-console.log('travel:', travel);
-
-var goal = prompt('Did I go to graduate school?');
-console.log('school goals:', goal);
-
-var tech = prompt('Do I work in the tech industry?');
-console.log('tech job:', tech);
-
-if (school.toUpperCase() === 'yes' || school === 'YES') {
-  console.log('Went to school:', 'great');
-  // do stuff here
+var snow = prompt('How many countries have I snowboarded in?');
+//console.log('snow goals:', goal);
+if (snow === '7' || snow.toLowerCase() === 'seven') {
+  alert('Yes, I have been to 7 different countries');
+} else {
+  alert('Wrong, I have been to 7 different countries');
 }
 
-if (school === 'n' && goal === 'y') {
-  console.log('Higher Education:', 'False')
->>>>>>> 567b18eb09448fcb37ce78ca0a50687fc3cb90ef
+var tech = prompt('Where did I start getting into coding?');
+//console.log('tech job:', tech);
+if (tech.toLowerCase() === 'Seattle' || tech.toLowerCase() === 'Seattle, Washington') {
+  alert('Yes, I started to code in Seattle!');
+} else {
+  alert('Wrong, I started in Seattle.' + tech);
 }
+
+document.getElementById('user').innerHTML = user;
+
+
